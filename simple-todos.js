fd112386d7344b50781ed9dd9fb864a645c183e5
Kohei TAKATA <kt.koheitakata@gmail.com>
2015-05-02 12:23:57 +0900

    4.Forms and events

--- simple-todos.js
+++ simple-todos.js
@@ -4,11 +4,25 @@ if (Meteor.isClient) {
   // counter starts at 0
   Template.body.helpers({
     tasks: function () {
-    	return Tasks.find({});
+    	return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+  
+  Template.body.events({
+    "submit .new-task": function (event) {
+      var text = event.target.text.value;
+      
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      
+      event.target.text.value = "";
+      
+      return false;
     }
   });
 }
-
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
